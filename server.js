3b982233ca8c897c5110ce24cceaf6676c13722d
Subhadeep Das <108357544+SUBHADEEP12345678@users.noreply.github.com>
2024-05-27 11:43:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/client/build/index.html') ));
 
 /** appliation port */
-const port = process.env.PORT || 8080;
+const port = process.env.PORT ;
 
 
 /** routes */
